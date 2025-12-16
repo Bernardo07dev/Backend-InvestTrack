4fe32ce71b917bcc50f3da2cb408f8bc2b8ba167
Bernardo <bernardomoreira.dev@gmail.com>
2025-12-16 18:33:09 -0300

    Cors + Alowed Hots

--- config/settings.py
+++ config/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'API',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,10 +72,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+CORS_ALLOWED_ORIGINS = [
+    "https://invest-track-sigma.vercel.app/",
+    "http://localhost:3000",
+]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
