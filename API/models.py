547744fb6e9ba0b682f3c62b68df0ac2d268d540
Bernardo <bernardomoreira.dev@gmail.com>
2025-12-19 11:05:46 -0300

    Invest front

--- API/models.py
+++ API/models.py
@@ -15,6 +15,7 @@ class Investimentos(models.Model):
     quantidade = models.IntegerField()
     data = models.DateField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.stock
